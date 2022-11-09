8763823a16ae87aaae9c90c34d97d4cd6cd131f2
Anna-Yavorska <aniayavorsk@gmail.com>
2022-11-09 10:00:05 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("We are glad to welcome you, " + args[0]);
+    }
+}
