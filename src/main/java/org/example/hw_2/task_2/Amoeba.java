c6d60fe68fd22c9ce99bd75cc50f640daeee2df7
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-13 18:34:08 +0100

    Update Amoeba.java

--- src/main/java/org/example/hw_2/task_2/Amoeba.java
+++ src/main/java/org/example/hw_2/task_2/Amoeba.java
@@ -3,8 +3,8 @@ package org.example.hw_2.task_1.task_2;
 public class Amoeba {
     public static void main(String[] args) {
         int ameoba = 1;
-        for (int i = 0; i <= 24; i = i + 3, ameoba *= 2) {
-            System.out.println("In " + i + " hr -" + " " + ameoba);
+        for (int i = 0; i <= 24; i = i + 3, ameoba *= 2) { // ameoba *= 2 убери в тело цикла, не стоит все писать в одну строчку
+            System.out.println("In " + i + " hr -" + " " + ameoba); // ameoba -> amoeba, стоит использовать слова на англ. языке, переводчик в этой ситуации помгает
         }
     }
 }
