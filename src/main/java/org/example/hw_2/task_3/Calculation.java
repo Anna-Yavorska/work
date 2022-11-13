f1d9991e896ef23ba28e92d40c0803fe62cf9e77
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-13 18:37:06 +0100

    Update Calculation.java

--- src/main/java/org/example/hw_2/task_3/Calculation.java
+++ src/main/java/org/example/hw_2/task_3/Calculation.java
@@ -2,7 +2,7 @@ package org.example.task_3;
 
 public class Calculation {
     public static void main(String[] args) {
-        int j = 0;
+        int j = 0; // j - это сумма, название переменной должно быть sum / summary
         for (int i = 0; i <= 8; i++) {
             j += 1 << i;
         }
