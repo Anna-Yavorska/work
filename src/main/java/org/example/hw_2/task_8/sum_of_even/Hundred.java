2a4ba7b4ebc469ba3bb73e3a9d7ef68396fbbb5a
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-13 18:49:30 +0100

    Update Hundred.java

--- src/main/java/org/example/hw_2/task_8/sum_of_even/Hundred.java
+++ src/main/java/org/example/hw_2/task_8/sum_of_even/Hundred.java
@@ -1,5 +1,6 @@
 package org.example.hw_2.task_1.task_8.sum_of_even;
 
+// это должен быть один класс на всю задачу, с одним циклом, внутри которого необходимо посчитать все числа и вывести на экран
 public class Hundred {
     public static void main(String[] args) {
         int j = 0;
