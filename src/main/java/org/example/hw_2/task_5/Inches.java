e6ef9195ec938ada515d47b849cb0de25e9b46e8
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-13 18:45:43 +0100

    Update Inches.java

--- src/main/java/org/example/hw_2/task_5/Inches.java
+++ src/main/java/org/example/hw_2/task_5/Inches.java
@@ -2,8 +2,8 @@ package org.example.task_5;
 
 public class Inches {
     public static void main(String[] args) {
-        double a = 2.54;
-        double b;
+        double a = 2.54; // 2.54 - это коэффициент перевода дюймов в сантиметры. Назови переменную в соответствии с тем, что в ней хранится. Название "а" не подходит
+        double b; // перенеси в 9 строку в скобки инициализации цикла
         int i = 1;
         System.out.println("Conversion from inches to centimeters");
         for (b = 1; b <= 20; b++) {
@@ -11,4 +11,6 @@ public class Inches {
             System.out.println(b * a);
         }
     }
+    
+    // на выходе не совсем таблица получилось, но в целом ок, а другие комментарии надо поправить
 }
