21fc7dc3607f3b71c718e062592f38004052cb97
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-13 18:47:46 +0100

    Update OddNumbers.java

--- src/main/java/org/example/hw_2/task_7/OddNumbers.java
+++ src/main/java/org/example/hw_2/task_7/OddNumbers.java
@@ -2,6 +2,7 @@ package org.example.hw_2.task_1.task_7;
 
 public class OddNumbers {
     public static void main(String[] args) {
+        //у  тебя программа выводит на экран все нечетные числа, а надо еще посчитать их сумму
         for (int i = 1; i <= 99; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
