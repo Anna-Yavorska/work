486ceb6a5c5144a64db1c836f2a95494226ed94c
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-16 22:25:39 +0100

    Update EvenElements.java

--- src/main/java/org/example/hw_3/task_6/EvenElements.java
+++ src/main/java/org/example/hw_3/task_6/EvenElements.java
@@ -2,6 +2,7 @@ package org.example.hw_3.task_6;
 
 import java.util.Arrays;
 
+//+
 public class EvenElements {
     public static void main(String[] args) {
         int[] numbers = {4, 8, 44, 55, 85, 82, 100, 8, 55};
@@ -11,7 +12,9 @@ public class EvenElements {
                 evenNumbers++;
             }
         }
+        
         System.out.println(Arrays.toString(numbers));
+        
         int[] evenArray = new int[evenNumbers];
         int index = 0;
         for (int i = 0; i < numbers.length; i++) {
