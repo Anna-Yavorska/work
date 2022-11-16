fe32a8acd22b677f70c2f7085e42994f3904f8a4
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-16 22:22:27 +0100

    Update MultiplicationTable.java

--- src/main/java/org/example/hw_3/task_4/MultiplicationTable.java
+++ src/main/java/org/example/hw_3/task_4/MultiplicationTable.java
@@ -4,8 +4,8 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         int[][] multiplication = new int[10][10];
         System.out.println("             Multiplication table");
-        System.out.println("I------------------------------------------------------------I");
-        System.out.println("I 1  II 2  II 3  II 4  II 5  II 6  II 7  II 8  II 9          I");
+        System.out.println("I------------------------------------------------------------I"); // красиво, но вообще непонятно :)
+        System.out.println("I 1  II 2  II 3  II 4  II 5  II 6  II 7  II 8  II 9          I"); // не привязывайся к конкретным числам, таблица может быть и до 100х100
         System.out.println("I------------------------------------------------------------I");
         for (int i = 1; i < multiplication.length; i++) {
             int[] firstMultiplication = multiplication[i];
