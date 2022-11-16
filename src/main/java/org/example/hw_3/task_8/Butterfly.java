a00c09195be5c7987bdac84e88ae35cbb8cca300
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-16 22:29:33 +0100

    Update Butterfly.java

--- src/main/java/org/example/hw_3/task_8/Butterfly.java
+++ src/main/java/org/example/hw_3/task_8/Butterfly.java
@@ -2,11 +2,12 @@ package org.example.hw_3.task_8;
 
 import java.util.Arrays;
 
+// идея с двумерным массивом хорошая, осталось придумать как ее красиво распечатать, возможно понадобиться два цикла - один для верхней части, один для нижней
 public class Butterfly {
     public static void main(String[] args) {
         int[][] wings = new int[9][];
         wings[0] = new int[]{1};
-        wings[1] = new int[]{1, 2};
+        wings[1] = new int[]{1, 2}; // не привязывайся к конкретным числам, можно задать другой размер бабочки и программа должна ее распечатать
         wings[2] = new int[]{1, 2, 3};
         wings[3] = new int[]{1, 2, 3, 4};
         wings[4] = new int[]{1, 2, 3, 4, 5};
