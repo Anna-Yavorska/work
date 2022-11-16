ca16bbafe5b0fd234a56092eb72f476de2e72062
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-16 22:20:31 +0100

    Update OddElements.java

--- src/main/java/org/example/hw_3/task_4/task_3/OddElements.java
+++ src/main/java/org/example/hw_3/task_4/task_3/OddElements.java
@@ -1,12 +1,13 @@
 package org.example.hw_3.task_4.task_3;
 
 import java.util.Arrays;
-
+ 
+// у тебя task_3 находится внутри task_4, скорее всего по ошибке, но надо исправить
 public class OddElements {
     public static void main(String[] args) {
         int[] numbers = {0, 2, 5, 0, 9, 12, 14, 45, 6, 4};
         boolean needToSort = true;
-        while (needToSort) {
+        while (needToSort) { // а зачем этот цикл? одного прохода с for будет достаточно
             needToSort = false;
             for (int i = 1; i < numbers.length; i++) {
                 if (numbers[i] % 2 != 0) {
