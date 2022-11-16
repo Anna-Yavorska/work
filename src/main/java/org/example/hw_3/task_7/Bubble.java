97ad5bf65a410f27050237ac62226e10447ed3ca
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-16 22:27:09 +0100

    Update Bubble.java

--- src/main/java/org/example/hw_3/task_7/Bubble.java
+++ src/main/java/org/example/hw_3/task_7/Bubble.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 public class Bubble {
     public static void main(String[] args) {
-        int[] unsortedArray = {0, 3, 14, 6, 15, 80, 96, 46};
+        int[] unsortedArray = {0, 3, 14, 6, 15, 80, 96, 46}; 
+        /* unsortedArray неплохое название, но в процессе программы ты его сортируешь, а он по-прежнему будет называться unsortedArray. 
+        Можно просто array
+        */
         System.out.println(Arrays.toString(unsortedArray));
         boolean needToSort = true;
         while (needToSort) {
