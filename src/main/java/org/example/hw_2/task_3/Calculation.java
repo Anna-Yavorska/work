a7160cf16d6c1e8e00c3d9d8a9a4ea661593c403
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-18 09:09:19 +0100

    Update Calculation.java

--- src/main/java/org/example/hw_2/task_3/Calculation.java
+++ src/main/java/org/example/hw_2/task_3/Calculation.java
@@ -4,7 +4,7 @@ public class Calculation {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 0; i <= 8; i++) {
-            sum += 1 << i;
+            sum += 1 << i; // аккуратнее с использованием <<, в данной задаче ок
         }
         System.out.println(sum);
     }
