2a2430a42795dcecb5e7e0a58ee59770d5ab7dbc
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-18 09:23:00 +0100

    Update Hundred.java

--- src/main/java/org/example/hw_2/task_8/hundred_sum/Hundred.java
+++ src/main/java/org/example/hw_2/task_8/hundred_sum/Hundred.java
@@ -6,6 +6,7 @@ public class Hundred {
         int sumOdd = 0;
         int totalSum = 0;
 
+        // в данном случае у тебя в итоге 100 итераций в двух циклах, что в целом ок, но зачем два цикла? Сделай один, в котором просто i++ и проверяй на ченость и нечетность
         for (int even = 2; even <= 100; even += 2) {
             sumEven += even;
             System.out.println(sumEven);
@@ -16,6 +17,6 @@ public class Hundred {
         }
         totalSum = sumEven + sumOdd;
         System.out.println(totalSum);
-
+// лишняя строка
     }
 }
