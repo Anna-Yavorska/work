53ab97ab2a8a33aee5c00b548d0760b40eb03f6b
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-20 16:36:56 +0100

    Update NameAge.java

--- src/main/java/org/example/hw_4/task_2/NameAge.java
+++ src/main/java/org/example/hw_4/task_2/NameAge.java
@@ -1,6 +1,6 @@
 package org.example.hw_4.task_2;
 
-public class NameAge {
+public class NameAge { // а если в класс добавить еще поля, то придется менять название? Придумай какое-нибудь другое, вариантов тонна: Рысь, Машина, Дерево...
 
     public String name = "Viktoria";
     public Integer age = 32;
@@ -19,7 +19,7 @@ public class NameAge {
         this.age = age;
     }
 
-    public NameAge() {
+    public NameAge() { // в задании сказано, что внутри конструктора должы присваиваться значения по умолчанию, твой вариант в целом тоже подходит
     }
 
     static {
