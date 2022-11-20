08eafe996f1a04b0e3440f053414053009d38288
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-20 16:40:48 +0100

    Update Array.java

--- src/main/java/org/example/hw_4/task_3/Array.java
+++ src/main/java/org/example/hw_4/task_3/Array.java
@@ -8,7 +8,7 @@ public class Array {
         nameAge[0] = new NameAge();
         nameAge[1] = new NameAge("Vlad", 40);
         for (int i = 0; i < 2; i++) {
-            System.out.println("Name = " + nameAge[i].name);
+            System.out.println("Name = " + nameAge[i].name); // внутри класса NameAge создай метод toString как мы делали на занятии и замени на System.out.println(nameAge[i])
             System.out.println("Age = " + nameAge[i].age);
         }
     }
