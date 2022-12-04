6d130cc520fe5c8951f2f4ae60d8919a079a0a51
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:07:42 +0100

    Update WarehouseMain.java

--- src/main/java/org/example/hw_6/task_2/WarehouseMain.java
+++ src/main/java/org/example/hw_6/task_2/WarehouseMain.java
@@ -2,8 +2,8 @@ package org.example.hw_6.task_2;
 
 public class WarehouseMain extends Warehouse {
     public static void main(String[] args) {
-        Warehouse warehouse = new WarehouseMain();
+        Warehouse warehouse = new WarehouseMain(); // в конструктор класса Warehouse надо передать массив
         warehouse.getArray();
-        warehouse.next();
+        warehouse.next(); // я бы предложил вызвать метод next несколько раз, чтобы проверить его работоспособность
     }
 }
