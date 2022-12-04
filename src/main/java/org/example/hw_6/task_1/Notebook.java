76320426402dd85728a5ad30411402f51fdb7609
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:02:00 +0100

    Update Notebook.java

--- src/main/java/org/example/hw_6/task_1/Notebook.java
+++ src/main/java/org/example/hw_6/task_1/Notebook.java
@@ -1,23 +1,23 @@
 package org.example.hw_6.task_1;
 
 public class Notebook implements Computer {
-    private final String userNaim = "Aleksey";
+    private final String userNaim = "Aleksey"; //userNaim -> у тебя опечатка и название переменной должно быть USER_NAME для констант + добавь static
 
     @Override
     public void On() {
         System.out.println("Welcome, " + userNaim + "!");
-
+//лишняя пустая строка
     }
 
     @Override
     public void Off() {
-
+//лишняя пустая строка
         System.out.println("Good bye, " + userNaim + "!");
     }
 
     @Override
     public void Reset() {
-
+//лишняя пустая строка
         System.out.println("Do you really want it, " + userNaim + "?");
     }
 }
