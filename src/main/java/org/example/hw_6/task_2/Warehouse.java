76bd8ca6ce1ce5541f0e47b7fed2b8b4baed7081
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:06:20 +0100

    Update Warehouse.java

--- src/main/java/org/example/hw_6/task_2/Warehouse.java
+++ src/main/java/org/example/hw_6/task_2/Warehouse.java
@@ -2,12 +2,13 @@ package org.example.hw_6.task_2;
 
 import java.util.Arrays;
 
+// отформатируй код
 public class Warehouse {
-   static String [] fruit = {"Melon", "Apple", "Mandarin", "Pear", "Banana"};
-    public void getArray(){
+   static String [] fruit = {"Melon", "Apple", "Mandarin", "Pear", "Banana"}; // поля класса должны быть приватными. Массив fruit не должен быть static
+    public void getArray(){ // в задании сказано, что метод getArray() должен возвращать массив, значит, метод не может быть void
         System.out.println(Arrays.toString(fruit));
-    }
-    public void next(){
+    } // абзац
+    public void next(){ // в задании сказано, что метод next() должен возвращать элемент массива, значит, метод не может быть void
         for (var i = 0; i < fruit.length; i++){
             System.out.println(fruit[i]);
     }
@@ -15,4 +16,6 @@ public class Warehouse {
             System.out.println(fruit[0]);
         }
     }
+   
+   // по заданию еще должен быть метод toString()
 }
