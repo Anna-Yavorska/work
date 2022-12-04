a7b67d2367e049b34820cb2104eb6d7a6bec6bb6
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:09:15 +0100

    Update Home.java

--- src/main/java/org/example/hw_6/task_3/Home.java
+++ src/main/java/org/example/hw_6/task_3/Home.java
@@ -2,15 +2,15 @@ package org.example.hw_6.task_3;
 
 public interface Home {
 
-    default void GetFloors() {
+    default void GetFloors() { // название методов с маленькой буквы и забываем про default методы в интерфейсе
         System.out.print("Floors =");
     }
 
-    default void TurnHeating() {
+    default void TurnHeating() { // название методов с маленькой буквы,
         System.out.print("Heating ");
     }
 
-    default void GetResidents() {
+    default void GetResidents() { // название методов с маленькой буквы,
         System.out.println("Residents = ");
     }
 }
