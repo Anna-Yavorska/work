dd04ac1f522a17648dcd99ddebdb6d6b02542add
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:00:16 +0100

    Update Computer.java

--- src/main/java/org/example/hw_6/task_1/Computer.java
+++ src/main/java/org/example/hw_6/task_1/Computer.java
@@ -1,15 +1,15 @@
 package org.example.hw_6.task_1;
 
 public interface Computer {
-    default void On() {
+    default void On() { // название методов с маленькой буквы, дефолтные методы в интерфейсе это больше лайв хак, чем хорошая практика, а поэтому лучше их всегда делать абстрактными
         System.out.println("Welcome!");
     }
 
-    default void Off() {
+    default void Off() { // название методов с маленькой буквы,
         System.out.println("Good bye!");
     }
 
-    default void Reset() {
+    default void Reset() { // название методов с маленькой буквы,
         System.out.println("Do you really want it?");
     }
 }
