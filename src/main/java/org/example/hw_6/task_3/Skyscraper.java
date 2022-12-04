15b181c91c9db741f49a0b24c56be9b97c71cf34
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-04 19:11:27 +0100

    Update Skyscraper.java

--- src/main/java/org/example/hw_6/task_3/Skyscraper.java
+++ src/main/java/org/example/hw_6/task_3/Skyscraper.java
@@ -1,19 +1,20 @@
 package org.example.hw_6.task_3;
 
+// если в начале метода используешь слово "get", то метод не может быть void
 public class Skyscraper implements Home {
 
     @Override
-    public void GetFloors() {
+    public void GetFloors() { // название методов с маленькой буквы,
         System.out.print("Floors = " + 15);
     }
 
     @Override
-    public void TurnHeating() {
+    public void TurnHeating() { // название методов с маленькой буквы,
         System.out.print(" Heating is individual ");
     }
 
     @Override
-    public void GetResidents() {
+    public void GetResidents() { // название методов с маленькой буквы,
         System.out.println("Residents = " + 5000);
     }
 }
