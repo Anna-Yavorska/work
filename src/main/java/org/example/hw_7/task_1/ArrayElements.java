3f3fffc19f533ddd9af587fe925b9290efbf9099
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-08 17:44:39 +0100

    Update ArrayElements.java

--- src/main/java/org/example/hw_7/task_1/ArrayElements.java
+++ src/main/java/org/example/hw_7/task_1/ArrayElements.java
@@ -2,11 +2,12 @@ package org.example.hw_7.task_1;
 
 public class ArrayElements {
     public static void main(String[] args) {
-        int[] five = new int[]{10, 20, 30, 40, 50, 60};
+        int[] five = new int[]{10, 20, 30, 40, 50, 60}; // five - почему такое название?
         int index = five.length;
-        arrayRecursion(five, index);
+        arrayRecursion(five, index); // arrayRecursion -> printArrayRecursion -> название метода - это глагол
     }
 
+    // твоя функция печатает массив в обратном порядке и немного неправильно написано, ниже оставил пример
     private static int arrayRecursion(int[] five, int index) {
         if (index <= 0) {
             return five[0];
@@ -15,4 +16,15 @@ public class ArrayElements {
         System.out.println(five[index]);
         return five[index];
     }
+    
+    /*
+        private static int arrayRecursion(int[] five, int index) {
+        if (index == five.length - 1) {
+            return five[index];
+        }
+        System.out.println(five[index]);
+
+        return arrayRecursion(five, ++index);
+    }
+    */
 }
