65f2b79c461d4869828a504fdadcd6b6c178ccb9
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-12 15:23:24 +0100

    Update Reflection.java

--- src/main/java/org/example/hw_8/task_2/Reflection.java
+++ src/main/java/org/example/hw_8/task_2/Reflection.java
@@ -2,7 +2,7 @@ package org.example.hw_8.task_2;
 
 public class Reflection { // это очень абстрактное название, попробуй добавить ему уникальности или просто назови как-нибудь не привязываясь к слову Reflection
 
-    private void printNameWater() { // хорошее название метода, думаю стоит добавить аргумент, который и будет названием
+    private void printNameWater() { // хорошее название метода, думаю стоит добавить аргумент, который и будет названием воды
         System.out.println("Borjomi");
     }
 }
