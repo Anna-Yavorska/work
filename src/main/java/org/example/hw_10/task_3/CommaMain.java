2075f5a1e52d6dccd9ce0fedb59b5620bfafc4b3
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-19 18:09:44 +0100

    Update CommaMain.java

--- src/main/java/org/example/hw_10/task_3/CommaMain.java
+++ src/main/java/org/example/hw_10/task_3/CommaMain.java
@@ -2,7 +2,7 @@ package org.example.hw_10.task_3;
 
 public class CommaMain {
     public static void main(String[] args) {
-        String coloursName = "Green. red. blue. yellow";
+        String coloursName = "Green. red. blue. yellow"; // coloursName -> colors
         coloursName = coloursName.replace(".", ",");
         System.out.println(coloursName);
     }
