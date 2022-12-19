dad04f06d4acd4573eca1dc0d089790343f93f8c
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-19 18:07:25 +0100

    Update DraculaMain.java

--- src/main/java/org/example/hw_10/task_2/DraculaMain.java
+++ src/main/java/org/example/hw_10/task_2/DraculaMain.java
@@ -2,7 +2,7 @@ package org.example.hw_10.task_2;
 
 public class DraculaMain {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("alucard");
+        StringBuilder stringBuilder = new StringBuilder("alucard"); // название переменной
         stringBuilder.reverse();
         System.out.println(stringBuilder);
     }
