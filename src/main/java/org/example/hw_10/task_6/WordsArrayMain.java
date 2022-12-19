71688f604f78b7db35b0f917d8d2e0bda3b31159
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-19 18:17:42 +0100

    Update WordsArrayMain.java

--- src/main/java/org/example/hw_10/task_6/WordsArrayMain.java
+++ src/main/java/org/example/hw_10/task_6/WordsArrayMain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class WordsArrayMain {
     public static void main(String[] args) {
-        String[] strings = new String[6];
+        String[] strings = new String[6]; // string -> words
         strings[0] = "Good";
         strings[1] = "Tree";
         strings[2] = "World";
@@ -12,13 +12,18 @@ public class WordsArrayMain {
         strings[4] = "lamp";
         strings[5] = "Wow";
 
+        // StringBuiler filteredWordsByComma;
         for (int i = 0; i < strings.length; i++) {
-            if (strings[i].startsWith("w")) {
+            if (strings[i].startsWith("w")) { // "w" - должно быть в переменной
                 System.out.println(strings[i]);
             }
-            if (strings[i].startsWith("W")) {
+            if (strings[i].startsWith("W")) { // "W" - должно быть в переменной
                 System.out.println(strings[i]);
             }
         }
+        
+        /*
+        String[] filteredWords;
+        */
     }
 }
