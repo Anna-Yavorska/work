e38edc4ea540c5f3e5815083ffabefcac5394c0f
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-06 08:18:11 +0100

    Update NewGladiator.java

--- src/main/java/org/example/hw_11/task_2/NewGladiator.java
+++ src/main/java/org/example/hw_11/task_2/NewGladiator.java
@@ -2,7 +2,7 @@ package org.example.hw_11.task_2;
 
 import java.util.Scanner;
 
-public class NewGladiator {
+public class NewGladiator { // не используй в названиях классов New
     static String[] attack = new String[]{"Strike in the head", "Strike in the body", "Strike in the legs"};
     static String[] protection = new String[]{"Put the head block", "Put the body block", "Put the legs block"};
     static int numberAttack = (int) Math.floor(Math.random() * attack.length);
