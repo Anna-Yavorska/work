ed557c13f2cf433eb8460222e4d72d55d0c1834d
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-12 17:25:12 +0100

    Update CheckArrayLengthException.java

--- src/main/java/org/example/hw_12/task_3/CheckArrayLengthException.java
+++ src/main/java/org/example/hw_12/task_3/CheckArrayLengthException.java
@@ -1,4 +1,4 @@
 package org.example.hw_12.task_3;
 
-public class CheckArrayLengthException extends Exception {
+public class CheckArrayLengthException extends Exception { // CheckArrayLengthException -> ArrayLengthException - название класса - это имя существительное, а не глагол
 }
