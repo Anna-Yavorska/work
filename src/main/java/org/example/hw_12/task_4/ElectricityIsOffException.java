446404801682fabe77f420f5220e01c4bd2dc599
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-12 17:34:19 +0100

    Update ElectricityIsOffException.java

--- src/main/java/org/example/hw_12/task_4/ElectricityIsOffException.java
+++ src/main/java/org/example/hw_12/task_4/ElectricityIsOffException.java
@@ -1,4 +1,4 @@
 package org.example.hw_12.task_4;
 
-public class ElectricityIsOffException extends Exception {
+public class ElectricityIsOffException extends Exception { // ElectricityIsOffException -> ElectricityException
 }
