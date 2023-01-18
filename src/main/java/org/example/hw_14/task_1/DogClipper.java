1cf8c0ec5aa8bd2e230aac680d889b7fa07eafa3
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-18 22:54:06 +0100

    Update DogClipper.java

--- src/main/java/org/example/hw_14/task_1/DogClipper.java
+++ src/main/java/org/example/hw_14/task_1/DogClipper.java
@@ -1,6 +1,12 @@
 package org.example.hw_14.task_1;
 
 public class DogClipper <T> extends Clipper<T>{
+    /*
+    у тебя Е и Т - это разные типы, получается бессмысленный дженерик, т.к. я могу сделать что-то вроде:
+    
+    DogClipper<Integer> integerClipper = new DogClipper<>(new String[] {}); - т.е. дженерик вроде Integer,
+    а передаю в конструктор массив String и это будет работать
+    */
     public <E> DogClipper(E[] type) {
         super(type);
     }
