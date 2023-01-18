8422dd560b671de135ec1075c84a6e89f829e27f
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-18 22:55:49 +0100

    Update SharedGarage.java

--- src/main/java/org/example/hw_14/task_2/SharedGarage.java
+++ src/main/java/org/example/hw_14/task_2/SharedGarage.java
@@ -1,5 +1,6 @@
 package org.example.hw_14.task_2;
 
+// У тебя класс Garage делает тоже самое, этот класс стоит удалить
 public class SharedGarage<T> extends Garage<T> {
     public SharedGarage(T[] carBrand) {
         super(carBrand);
