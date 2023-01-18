efa25d38eb05b06eb84e9e6e629dd6fafef58394
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-18 22:48:26 +0100

    Update Clipper.java

--- src/main/java/org/example/hw_14/task_1/Clipper.java
+++ src/main/java/org/example/hw_14/task_1/Clipper.java
@@ -1,9 +1,10 @@
 package org.example.hw_14.task_1;
 
+// а зачем каждый раз наследоваться от Clipper? Ты можешь создать поле типа Т и оставить typeOfClipper
 public class Clipper<T> {
     private String typeOfClipper;
 
-    public <E> Clipper(E[] type) {
+    public <E> Clipper(E[] type) { // Можно сделать и массив, но в задании был один аргумент. В твоем случае тип всегда будет "массив чего-нибудь"
         typeOfClipper = type.getClass().getName();
     }
 
