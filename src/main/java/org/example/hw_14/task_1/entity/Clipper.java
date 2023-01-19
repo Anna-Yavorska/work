32cc13d61f67488f6c3d96d8a3072c336ec5c999
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-19 17:30:41 +0100

    Update Clipper.java

--- src/main/java/org/example/hw_14/task_1/entity/Clipper.java
+++ src/main/java/org/example/hw_14/task_1/entity/Clipper.java
@@ -1,6 +1,7 @@
 package org.example.hw_14.task_1.entity;
 
-public class Clipper<T> {
+public class Clipper<T> { // этот класс не подходит под понятие сущность (entity). Это скорее класс Service, который выполняет какую-либо логику.
+    // Думаю даже можно его переименовать в ClipperService
     private T typeOfClipper;
 
     public Clipper(T typeOfClipper) {
@@ -8,7 +9,7 @@ public class Clipper<T> {
     }
 
     public String getTypeOfClipper() {
-        String type = typeOfClipper.getClass().getName();
+        String type = typeOfClipper.getClass().getName(); // можно сократить до return typeOfClipper.getClass().getName();
         return type;
     }
 }
