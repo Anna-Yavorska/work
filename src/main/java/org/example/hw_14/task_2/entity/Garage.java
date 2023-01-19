f88afacf49e4b41638be79f47e5b3091be6fa682
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-19 17:33:44 +0100

    Update Garage.java

--- src/main/java/org/example/hw_14/task_2/entity/Garage.java
+++ src/main/java/org/example/hw_14/task_2/entity/Garage.java
@@ -1,6 +1,7 @@
 package org.example.hw_14.task_2.entity;
 
-public class Garage<Car> {
+public class Garage<Car> { // у тебя Car - это название интерфейса, лучше не использовать одинаковые названия. 
+    // Тем более по факту с таким generic у тебя в гараже может быть абсолютно любой класс
     private Car[] carBrand;
 
     public Garage(Car[] carBrand) {
