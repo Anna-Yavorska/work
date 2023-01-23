9b24ff0ab21796bafa66b6af163708aa33bf0ef1
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-23 17:23:14 +0100

    Update WholeNumbers.java

--- src/main/java/org/example/hw_15/task_2/WholeNumbers.java
+++ src/main/java/org/example/hw_15/task_2/WholeNumbers.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+//+
 public class WholeNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
