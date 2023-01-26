4009b848810cbafcbf43d0a4244cc58da90dd03b
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-26 16:52:49 +0100

    Update NumberComparator.java

--- src/main/java/org/example/hw_16/task_1/NumberComparator.java
+++ src/main/java/org/example/hw_16/task_1/NumberComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class NumberComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
-        return -1 * o1.compareTo(o2);
+        return -1 * o1.compareTo(o2); // я бы предложил убрать -1 а при создании этого компаратора вызвать метод reversed(). Так будет меньше путаницы
     }
 }
