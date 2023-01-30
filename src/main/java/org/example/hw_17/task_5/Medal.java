91262e49574ef9eeb48b8b7a53dd3da88c8da975
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-30 08:51:41 +0100

    Update Medal.java

--- src/main/java/org/example/hw_17/task_5/Medal.java
+++ src/main/java/org/example/hw_17/task_5/Medal.java
@@ -3,8 +3,8 @@ package org.example.hw_17.task_5;
 public enum Medal {
     GOLD(25, 1),
     SILVER(64, 2),
-    BRONZE(2, 3);
-    private Integer raceNumber;
+    BRONZE(2, 3); // абзац
+    private Integer raceNumber; // номер забега вряд ли может быть в енам, т.к. это скорее всего число, которое будет часто обновляться
     private Integer place;
 
     Medal(Integer raceNumber, Integer place) {
