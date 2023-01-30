b395cdc3660c16dffb33e8144f516192cf9f597b
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-30 08:30:04 +0100

    Update ConvertingNumberToStringMain.java

--- src/main/java/org/example/hw_17/task_1/ConvertingNumberToStringMain.java
+++ src/main/java/org/example/hw_17/task_1/ConvertingNumberToStringMain.java
@@ -4,7 +4,7 @@ public class ConvertingNumberToStringMain {
 
     public static void main(String[] args) {
         ConvertingService convertingService = new ConvertingService();
-        Converting converting = convertingService::convertToString;
+        Converting converting = convertingService::convertToString; // Converting этот интерфейс лучше заменить на Function<Integer, String>
 
         String convertingResult = converting.converting(1100);  // convertingResult -> result будет достаточно в данном случае
         System.out.println(convertingResult);
