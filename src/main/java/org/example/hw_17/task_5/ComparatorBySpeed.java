c04450284417662e0f05ae540df6312e741a7f31
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-30 08:53:55 +0100

    Update ComparatorBySpeed.java

--- src/main/java/org/example/hw_17/task_5/ComparatorBySpeed.java
+++ src/main/java/org/example/hw_17/task_5/ComparatorBySpeed.java
@@ -2,7 +2,7 @@ package org.example.hw_17.task_5;
 
 import java.util.Comparator;
 
-public class ComparatorBySpeed implements Comparator<Sportsman> {
+public class ComparatorBySpeed implements Comparator<Sportsman> { // SportsmanSpeedComparator
     @Override
     public int compare(Sportsman o1, Sportsman o2) {
         return o2.getSpeed().compareTo(o1.getSpeed());
