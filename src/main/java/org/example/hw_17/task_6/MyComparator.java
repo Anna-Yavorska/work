fd312f90263d4af90668d4bf62319e29920afd44
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-30 09:32:33 +0100

    Update MyComparator.java

--- src/main/java/org/example/hw_17/task_6/MyComparator.java
+++ src/main/java/org/example/hw_17/task_6/MyComparator.java
@@ -2,7 +2,7 @@ package org.example.hw_17.task_6;
 
 import java.util.List;
 
-public interface MyComparator {
+public interface MyComparator { // MyComparator - уже стыдно так интерфейсы называть
 
     List<String> tornOver(List<String> strings);
 }
