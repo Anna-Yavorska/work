873876c847949ccfd7d4ccc7d778149891d9d781
Alexander Fomin <RezviyBelorus@gmail.com>
2023-01-30 09:38:49 +0100

    Update ComparatorService.java

--- src/main/java/org/example/hw_17/task_6/ComparatorService.java
+++ src/main/java/org/example/hw_17/task_6/ComparatorService.java
@@ -5,7 +5,14 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ComparatorService {
-    public List<String> turn(List<String> strings) {
+    public List<String> turn(List<String> strings) { // у тебя метод просто меняет порядок расположения, без сортировки. 
+        /*
+        Iterator здесь не нужен. Если делать руками, то достаточно двух методов
+        strings.get();
+        strings.set() - по индексу заменяет элемент.
+        А есть вариант еще проще:
+        Collections.reverse(strings);
+        */
         Iterator<String> iterator = strings.iterator();
         List<String> turnResult = new ArrayList<>();
         while (iterator.hasNext()) {
